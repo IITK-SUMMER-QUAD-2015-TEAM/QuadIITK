174cb90e464213caf08bf72c86582014d2d3464e
rachitiitk <rachitrocker99@gmail.com>
2015-06-08 17:32:52 +0530

    Update magnetometer.ino

--- code/magnetometer.ino
+++ code/magnetometer.ino
@@ -40,6 +40,12 @@ void getMagnet(void)
    
 }
 
+void calibrateMagnetometer(){
+     magnetVal[XAXIS]=M1*(magnetRaw[XAXIS]-B1)+M2*(magnetRaw[YAXIS]-B2)+M3*(magnetRaw[ZAXIS]-B3);
+     magnetVal[YAXIS]=M4*(magnetRaw[XAXIS]-B1)+M5*(magnetRaw[YAXIS]-B2)+M3*(magnetRaw[ZAXIS]-B3);
+     magnetVal[ZAXIS]=M7*(magnetRaw[XAXIS]-B1)+M8*(magnetRaw[YAXIS]-B2)+M9*(magnetRaw[ZAXIS]-B3);
+}
+
 void printMagnet(void)
 {
   Serial.print(magnetRaw[XAXIS]);Serial.print('\t');
@@ -48,7 +54,7 @@ void printMagnet(void)
 }
 
 void getHeading(){
-  heading=(float)atan2((float)magnetRaw[YAXIS],(float)magnetRaw[XAXIS])*180/PI;
+  heading=(float)atan2((float)magnetVal[YAXIS],(float)magnetVal[XAXIS])*180/PI;
  
   heading+=DEC_ANGLE;
   
